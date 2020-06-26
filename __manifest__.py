9f29f7f51083c6c7f076aa1f8a192ef088bcee25
EDallas89 <inmsanz89@gmail.com>
2020-06-26 09:37:17 +0100

    [ADD] Initial Commit, Duplicate Funcional

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Duplicate Calendar Event",
+    'summary': """
+        Add duplicate option on calendar events""",
+    'author': "Inma Sánchez",
+    'website': "https://github.com/EDallas89",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['calendar'],
+    'data': [
+        'views/calendar_event.xml',]
+}
